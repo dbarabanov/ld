c77c3960820c5f8294e72555fa81cdef8d683af2
Dmitri Barabanov <dmitri.barabanov@gmail.com>
2013-08-26 14:04:29 -0700

    rearranging result channel in increasing order

--- engine.go
+++ engine.go
@@ -1,7 +1,7 @@
 package ld
 
 import (
-	//"fmt"
+	"fmt"
 	"math"
 )
 
@@ -22,14 +22,17 @@ func (e engine) Run(chVariant chan *Variant) chan *Result {
 }
 
 func runEngine(chVariant chan *Variant, chResult chan *Result, params EngineParameters) {
-	//var workQueue chan *variantList
-	//TODO expose queue size somewhere (instead of hardcoding "2")
-	//workQueue := make(chan *variantList, params.NumWorkers*2) //make queue a little larger so that workers always have work.
 	reorderQueue := startReorderQueue(&params, chResult)
 	workQueue := startWorkers(&params, reorderQueue)
 	var head *variantList
 	tail := head
+	var last *Variant
 	for v := range chVariant {
+		if last != nil && last.Pos > v.Pos {
+			panic(fmt.Sprintf("variants out of order: %v(pos %v) followed by %v(pos %v)", last.Rsid, last.Pos, v.Rsid, v.Pos))
+		}
+		last = v
+		//fmt.Printf("last: %v v: %v\n", last, v)
 		//fmt.Printf("v: %v\n", v)
 		if head == nil {
 			head = &variantList{0, v, nil}
@@ -44,27 +47,26 @@ func runEngine(chVariant chan *Variant, chResult chan *Result, params EnginePara
 			tail = tail.next
 		}
 	}
-	for tail != nil { //surve up the tail
+	for tail != nil { //serve up the tail
 		workQueue <- tail
 		tail = tail.next
 	}
 	close(workQueue)
 }
 
-func startReorderQueue(params *EngineParameters, chResult chan *Result) (reorderQueue chan *Result) {
-	reorderQueue = make(chan *Result, params.NumWorkers*10)
-	go reorderResults(reorderQueue, params.NumWorkers, chResult)
-	return reorderQueue
+func startReorderQueue(params *EngineParameters, chResult chan *Result) (chReorder chan *Result) {
+	chReorder = make(chan *Result, params.NumWorkers*10) //make reorder queue large enough so workers don't get blocked on output. TODO: expose constant 10
+	go reorderResults(chReorder, params.NumWorkers, chResult)
+	//go dontReorderResults(chReorder, params.NumWorkers, chResult)
+	return chReorder
 }
 
-func startWorkers(params *EngineParameters, chResult chan *Result) (workQueue chan *variantList) {
-	//TODO expose queue size somewhere (instead of hardcoding "5")
-	workQueue = make(chan *variantList, params.NumWorkers*5) //make queue a little larger so that workers always have work.
-	var i uint16
-	for i = 0; i < params.NumWorkers; i++ {
-		go runWorker(workQueue, chResult, params)
+func startWorkers(params *EngineParameters, chResult chan *Result) (chWork chan *variantList) {
+	chWork = make(chan *variantList, params.NumWorkers*10) //make work channel a multichannel so that workers always have work. TODO: expose constant 10
+	for i := uint16(0); i < params.NumWorkers; i++ {
+		go runWorker(chWork, chResult, params)
 	}
-	return workQueue
+	return chWork
 }
 
 func runWorker(in chan *variantList, out chan *Result, params *EngineParameters) {
@@ -84,7 +86,7 @@ func runWorker(in chan *variantList, out chan *Result, params *EngineParameters)
 	out <- nil
 }
 
-func reorderResults(in chan *Result, numWorkers uint16, out chan *Result) {
+func dontReorderResults(in chan *Result, numWorkers uint16, out chan *Result) {
 	var finishedWorkers uint16
 	for r := range in {
 		if r == nil {
@@ -95,6 +97,78 @@ func reorderResults(in chan *Result, numWorkers uint16, out chan *Result) {
 			}
 		} else {
 			out <- r
+		}
+	}
+}
+
+func reorderResults(in chan *Result, numWorkers uint16, out chan *Result) {
+	var finishedWorkers uint16
+	type resultList struct {
+		prev   *resultList
+		result *Result
+		next   *resultList
+	}
+	type resultQueue struct {
+		tail  *resultList
+		head  *resultList
+		depth uint32
+	}
+	queue := resultQueue{nil, nil, 0}
+	limit := uint32(numWorkers * 20)
+
+	for r := range in {
+		if r == nil {
+			finishedWorkers++
+			if finishedWorkers >= numWorkers {
+				for queue.tail != nil {
+					//fmt.Printf("tail%v depth: %v\n", queue.tail, queue.depth)
+					out <- queue.tail.result
+					queue.tail = queue.tail.next
+					queue.depth--
+				}
+				close(out)
+				break
+			}
+		} else {
+			if queue.head == nil {
+				queue.head = &resultList{nil, r, nil}
+				queue.tail = queue.head
+				queue.depth++
+			} else {
+				current := queue.head
+				//for current != queue.tail && current.result.Variant.Pos > r.Variant.Pos {
+				for current != nil && current.result.Variant.Pos > r.Variant.Pos {
+					current = current.prev
+				}
+				if current != nil {
+					//fmt.Printf("head: %v current: %v\n", queue.head, current)
+					//fmt.Printf("head: %v current: %v\n", queue.head.result.Variant.Pos, current.result.Variant.Pos)
+					next := current.next
+					current.next = &resultList{current, r, next}
+					if next != nil {
+						next.prev = current.next
+					}
+				} else {
+					queue.tail = &resultList{nil, r, queue.tail}
+					queue.tail.next.prev = queue.tail
+				}
+
+				if queue.head.next != nil {
+					queue.head = queue.head.next
+				}
+				queue.depth++
+				//fmt.Printf("queue(%v): ", queue.depth)
+				//for cur := queue.tail; cur != nil; cur = cur.next {
+				//fmt.Printf("%v ", cur.result.Variant.Pos)
+				//}
+				//fmt.Printf("\n")
+			}
+			if queue.depth >= limit {
+				out <- queue.tail.result
+				queue.tail = queue.tail.next
+				queue.tail.prev = nil
+				queue.depth--
+			}
 		}
 	}
 }
@@ -137,8 +211,8 @@ func union(a []uint32, b []uint32) (c []uint32) {
 func ComputeR2(a []uint32, b []uint32, bitLength uint16) (r2 float64) {
 	pAB := bitCount(union(a, b))
 	pa, pb := bitCount(a), bitCount(b)
-	if pa == 0 || pb == 0 || pa == bitLength || pb == bitLength {
-		return -1
+	if pa == 0 || pb == 0 || pa == bitLength || pb == bitLength { //if any of this is true, r2 formula is undefined. so just return 0. TODO: document this logic.
+		return 0
 	}
 	//fmt.Printf("pa: %v, pb: %v, pAB: %v, bitLength: %v\n", pa, pb, pAB, bitLength)
 	return round(calculateR2(int64(pa), int64(pb), int64(pAB), int64(bitLength)), 6)
